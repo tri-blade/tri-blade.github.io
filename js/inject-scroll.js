26ddc83d05a5173c0adc3cd34c2725aac3d73a75
tri-blade <r4v3nblaze@gmail.com>
2024-09-10 02:06:16 +0800

    Site updated: 2024-09-10 02:06:15

--- js/inject-scroll.js
+++ js/inject-scroll.js
@@ -1,25 +1,14 @@
         document.addEventListener('DOMContentLoaded', function() {
             var header = document.getElementById('page-header');
-            var content = document.getElementById('content-inner');
 
-            // 确保 header 和 content 都存在
-            if (header && content) {
-                window.addEventListener('wheel', function(event) {
-                    var delta = event.deltaY;  // 获取鼠标滚轮的滚动量
-                    var headerRect = header.getBoundingClientRect();
-                    var contentRect = content.getBoundingClientRect();
+            header.addEventListener('wheel', function(event) {
+                event.preventDefault();  // 阻止默认滚动行为
+                var delta = event.deltaY;
 
-                    // 在header内向下滚动
-                    if (delta > 0 && headerRect.bottom > 0) {
-                        event.preventDefault();
-                        content.scrollIntoView({ behavior: 'smooth' });
-                    }
-
-                    // 在content的顶部向上滚动
-                    if (delta < 0 && window.scrollY <= contentRect.top) {
-                        event.preventDefault();
-                        window.scrollTo({ top: 0, behavior: 'smooth' });
-                    }
-                });
-            }
+                if (delta > 0) {  // 向下滚动
+                    document.getElementById('content-inner').scrollIntoView({ behavior: 'smooth' });
+                } else if (delta < 0) {  // 向上滚动
+                    window.scrollTo({ top: 0, behavior: 'smooth' });
+                }
+            });
         });
