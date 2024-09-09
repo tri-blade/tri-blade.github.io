0b7599c266c751530ecda0c6ac8642071ad17e55
tri-blade <r4v3nblaze@gmail.com>
2024-09-10 00:31:04 +0800

    Site updated: 2024-09-10 00:31:03

--- js/live2d-loader.js
+++ js/live2d-loader.js
@@ -1,7 +1,2 @@
-import 'https://cdn.jsdelivr.net/npm/greensock@1.20.2/dist/TweenLite.js';
-import 'https://cubism.live2d.com/sdk-web/cubismcore/live2dcubismcore.min.js';
-import 'https://cdn.jsdelivr.net/npm/pixi.js@5.3.6/dist/pixi.min.js';
-import 'https://cdn.jsdelivr.net/npm/pixi-live2d-display@0.3.1/dist/cubism4.min.js';
-import 'https://cdn.jsdelivr.net/gh/journey-ad/blog-img@76ba2b3/live2d/lib/pio.js';
 import 'https://cdn.jsdelivr.net/gh/journey-ad/blog-img@76ba2b3/live2d/lib/pio_sdk4.js';
 import 'https://cdn.jsdelivr.net/gh/journey-ad/blog-img@76ba2b3/live2d/lib/load.js';
