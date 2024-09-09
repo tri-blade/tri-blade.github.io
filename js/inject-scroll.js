f6c4b1424631c81d46a3b0d8a4b701a5e7b18a2c
tri-blade <r4v3nblaze@gmail.com>
2024-09-10 02:14:49 +0800

    Site updated: 2024-09-10 02:14:48

--- js/inject-scroll.js
+++ js/inject-scroll.js
@@ -2,8 +2,18 @@
             var header = document.getElementById('page-header');
 
             header.addEventListener('wheel', function(event) {
-                event.preventDefault();  // Prevent default scroll behavior
-                // Always scroll to the content-inner regardless of the scroll direction
-                document.getElementById('content-inner').scrollIntoView({ behavior: 'smooth' });
+                event.preventDefault();  // 阻止默认滚动行为
+                var delta = event.deltaY;
+                var rect = header.getBoundingClientRect();
+                var middleY = rect.top + rect.height / 2;
+                var mouseY = event.clientY; // 获取鼠标在视口中的Y坐标
+
+                if (mouseY <= middleY) { // 只有当鼠标在header的中间部分以上时触发
+                    if (delta > 0) {  // 向下滚动
+                        document.getElementById('content-inner').scrollIntoView({ behavior: 'smooth' });
+                    } else if (delta < 0) {  // 向上滚动
+                        window.scrollTo({ top: 0, behavior: 'smooth' });
+                    }
+                }
             });
         });
