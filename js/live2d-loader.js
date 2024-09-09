906a86fc1b4035697d2a9b1c14173da1ddddbfa7
tri-blade <r4v3nblaze@gmail.com>
2024-09-10 00:17:49 +0800

    Site updated: 2024-09-10 00:17:48

---
+++ js/live2d-loader.js
@@ -0,0 +1,3 @@
+// 在一个新的 JavaScript 文件中（例如 live2d-loader.js）
+import 'https://cdn.jsdelivr.net/gh/journey-ad/blog-img@76ba2b3/live2d/lib/pio_sdk4.js';
+import 'https://cdn.jsdelivr.net/gh/journey-ad/blog-img@76ba2b3/live2d/lib/load.js';
