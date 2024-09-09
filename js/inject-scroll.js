01bdb868729d90c8f23a388978b38925db9b187c
tri-blade <r4v3nblaze@gmail.com>
2024-09-10 02:22:19 +0800

    Site updated: 2024-09-10 02:22:18

--- js/inject-scroll.js
+++ js/inject-scroll.js
@@ -4,10 +4,10 @@
             header.addEventListener('wheel', function(event) {
                 var delta = event.deltaY;
                 var rect = header.getBoundingClientRect();
-                var targetY = rect.top + rect.height*99 / 100;
+                var targetY = rect.top + rect.height;
                 var mouseY = event.clientY; // 获取鼠标在视口中的Y坐标
 
-                if (mouseY <= targetY) { // 只有当鼠标在header的中间部分以上时触发
+                if (mouseY <= targetY) { 
                     if (delta > 0) {  // 向下滚动
                         document.getElementById('content-inner').scrollIntoView({ behavior: 'smooth' });
                     } else if (delta < 0) {  // 向上滚动
